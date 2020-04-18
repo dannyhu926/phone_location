4d40fe4338a83fb33f1b61522dd6d1eda8e3ef2b
hudeyong <hudeyong@icsoc.net>
2020-04-19 00:24:43 +0800

    加入第三方接口

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # phone_location.sql
 手机号码归属地数据库，2019年437345条数据  手机归属地查询，运营商，邮编，区号。
-数据库收录未知的号码，通过第三方网站补充完善手机号码归属地数据库
-未知号码列表sql：select count(1) from phone_location where isp is null
+
+数据库收录未知的号码：
+
+1）通过第三方网站补充完善手机号码归属地数据库
+
+2）第三方接口查询
+
+未知号码列表sql：
+select * from phone_location where isp is null
 https://www.qqzeng.com/phone/
 
 how 2 use:
+
 select * from phone_location where phone like "1337785%" limit 1;
 
 #region.sql
 行政区划数据库_with+经纬度-省市区-邮编-区号-拼音-简称 3750条数据
+
 select city_code from region where level=2
 
 
---
+++ phpApi.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * 示例中sign会不定期调整
+ * https://www.nowapi.com/api/phone.get
+ {
+    "success":"1",
+    "result":{
+        "status":"ALREADY_ATT",
+        "phone":"13800138000", /*查询的手机号*/
+        "area":"010", /*区号*/
+        "postno":"100000", /*邮编*/
+        "att":"中国,北京", /*归属地样式1*/
+        "ctype":"北京移动全球通卡", /*卡类型*/
+        "par":"1380013", /*手机号前缀*/
+        "prefix":"138", /*号段*/
+        "operators":"移动", /*所属运营商*/
+        "style_simcall":"中国,北京", /*归属地样式1*/
+        "style_citynm":"中华人民共和国,北京市" /*归属地样式2*/
+    }
+}
+ */
+$demoKey = '10003';
+$demoSign = 'b59bc3ef6191eb9f747dd4e83c99f2a4';
+do {
+	$sql = "select phone from phone_location where isp is not null and city_code is null and flag=0 limit 100";
+	$list = $this->conn->fetchAll($sql);
+	foreach ($list as $info) {
+		$phonePref = $info['phone'];
+		$url = sprintf("http://api.k780.com/?app=phone.get&phone=%d1111&appkey=%s&sign=%s&format=json", $phonePref, $demoKey, $demoSign);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		$result = curl_exec($ch);
+		$arr = json_decode($result, true);
+		if ($arr['success']) {
+			$rs = $arr['result'];
+			if ($rs['status'] == 'ALREADY_ATT') {
+				$cityArr = explode(',', $rs['style_simcall']);
+				$sql = "update phone_location set city_code='{$rs['area']}', post_code='{$rs['postno']}',province='{$cityArr['1']}',city='{$cityArr['2']}',pref='{$rs['prefix']}', flag=1 where phone='$phonePref'";
+				$output->writeln($sql);
+				$this->conn->exec($sql);
+			}
+		}
+	}
+} while (!empty($list));
+
