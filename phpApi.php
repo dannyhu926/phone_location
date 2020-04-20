1e27ef170409db61b47e67e4c4ecb38be9aa938f
hudeyong <hudeyong@icsoc.net>
2020-04-20 10:13:36 +0800

    优化

--- phpApi.php
+++ phpApi.php
@@ -50,7 +50,8 @@ do {
             $rs = $arr['result'];
             if ($rs['status'] == 'ALREADY_ATT') {
                 $cityArr = explode(',', $rs['style_simcall']);
-                $sql = "update phone_location set city_code='{$rs['area']}',isp='{$rs['operators']}', post_code='{$rs['postno']}',province='{$cityArr['1']}',city='{$cityArr['2']}',pref='{$rs['prefix']}', flag=1 where phone='$phonePref'";
+                $city = $cityArr['2'] ?? $cityArr['1'];
+                $sql = "update phone_location set city_code='{$rs['area']}',isp='{$rs['operators']}', post_code='{$rs['postno']}',province='{$cityArr['1']}',city='{$city}',pref='{$rs['prefix']}', flag=1 where phone='$phonePref'";
                 $output->writeln($sql);
                 $this->conn->exec($sql);
             }
