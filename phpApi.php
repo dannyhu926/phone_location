677dd5d4c0cb064a2bf29f8efa127c278b7a284b
hudeyong <hudeyong@icsoc.net>
2020-04-27 11:22:30 +0800

    优化代码

--- phpApi.php
+++ phpApi.php
@@ -1,64 +1,37 @@
 <?php
 /**
- * 示例中sign会不定期调整
- * https://www.nowapi.com/api/phone.get
- * {
- * "success":"1",
- * "result":{
- * "status":"ALREADY_ATT",
- * "phone":"13800138000", //查询的手机号
- * "area":"010", //区号
- * "postno":"100000", //邮编
- * "att":"中国,北京", //归属地样式1
- * "ctype":"北京移动全球通卡", //卡类型
- * "par":"1380013", //手机号前缀
- * "prefix":"138", //号段
- * "operators":"移动", //所属运营商
- * "style_simcall":"中国,北京", //归属地样式1
- * "style_citynm":"中华人民共和国,北京市" //归属地样式2
- * }
- * }
+ * https://github.com/jonnywang/phone PHP手机归属地扩展
+ *
+ * 0 省份 1城市 2邮编 3区号 4号码类型（1移动 2联通 3电信 4电信虚拟运营商 5联通虚拟运营商 6移动虚拟运营商）
  */
-function callApi($phonePref, $demoKey, $demoSign)
-{
-    $url = sprintf(
-        "http://api.k780.com/?app=phone.get&phone=%d1111&appkey=%s&sign=%s&format=json",
-        $phonePref,
-        $demoKey,
-        $demoSign
-    );
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    $result = curl_exec($ch);
-    $arr = json_decode($result, true);
-
-    return $arr;
-}
-
-$demoKey = '10003';
-$demoSign = 'b59bc3ef6191eb9f747dd4e83c99f2a4';
-do {
-    $sql = "select phone from phone_location where isp is not null and city_code is null and flag=0 limit 100";
-    $list = $this->conn->fetchAll($sql);
-    foreach ($list as $info) {
-        $phonePref = $info['phone'];
-        $arr = callApi($phonePref, $demoKey, $demoSign);
-        if ($arr['success']) {
-            $rs = $arr['result'];
-            if ($rs['status'] == 'ALREADY_ATT') {
-                $cityArr = explode(',', $rs['style_simcall']);
-                $city = $cityArr['2'] ?? $cityArr['1'];
-                $sql = "update phone_location set city_code='{$rs['area']}',isp='{$rs['operators']}', post_code='{$rs['postno']}',province='{$cityArr['1']}',city='{$city}',pref='{$rs['prefix']}', flag=1 where phone='$phonePref'";
-                $output->writeln($sql);
-                $this->conn->exec($sql);
-            }
-        } else {
-            $sql = "update phone_location set flag=1 where phone='$phonePref'";
-            $output->writeln($sql);
-            $this->conn->exec($sql);
+$phone = '1367152';
+$sql = "select phone from phone_location where phone='$phone'";
+$info = $this->conn->fetchRow($sql);
+if (empty($info)) {
+    $info = phone($phone);//需要按照扩展
+    if ($info) {
+        $pref = substr($phone, 0, 3);
+        $sql = "insert into phone_location(pref,phone,province,city,isp,post_code,city_code) values(%s,%s,%s,%s,%s,%s,%s)";
+        if (1 == $info['4']) {
+            $isp = '移动';
         }
+        if (2 == $info['4']) {
+            $isp = '联通';
+        }
+        if (3 == $info['4']) {
+            $isp = '电信';
+        }
+        if (4 == $info['4']) {
+            $isp = '电信/虚拟';
+        }
+        if (5 == $info['4']) {
+            $isp = '联通/虚拟';
+        }
+        if (6 == $info['4']) {
+            $isp = '移动/虚拟';
+        }
+        $sql = sprintf($sql, $pref, $phone, $info['0'], $info['1'], $isp, $info['2'], $info['3']);
+        $this->conn->exec($sql);
     }
-} while (!empty($list));
-
+}
+var_dump($info);
